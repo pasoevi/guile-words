92ec0f27a02305767a2149a27cb2697eca27cfb1
Sergi Pasoev <s.pasoev@gmail.com>
2016-01-20 23:12:49 +0400

    glosbe api

--- apis.scm
+++ apis.scm
@@ -1,8 +1,8 @@
 (use-modules (web client))
 
 (define base-urls
-  '((#:glosbe . "http://api.wordnik.com/v4/word.json/{word}/{action}?api_key=1e940957819058fe3ec7c59d43c09504b400110db7faa0509")
-    (#:wordnik . "http://api.wordnik.com/v4/word.json/{word}/{action}?api_key=1e940957819058fe3ec7c59d43c09504b400110db7faa0509")
-    (#:urbandict . "http://api.urbandictionary.com/v0/{action}?term={word}")
-    (#:bighugelabs . "http://words.bighugelabs.com/api/2/eb4e57bb2c34032da68dfeb3a0578b68/{word}/json")
+  '((#:glosbe . "https://glosbe.com/gapi/translate?from=en&dest=en&format=json&pretty=true&phrase=~a")
+    (#:wordnik . "http://api.wordnik.com/v4/word.json/~a/~a?api_key=1e940957819058fe3ec7c59d43c09504b400110db7faa0509")
+    ;; (#:urbandict . "http://api.urbandictionary.com/v0/{action}?term={word}")
+    ;;(#:bighugelabs . "http://words.bighugelabs.com/api/2/eb4e57bb2c34032da68dfeb3a0578b68/~a/json")
     ))
--- words.scm
+++ words.scm
@@ -1,4 +1,7 @@
-(use-modules (json))
+(use-modules (web client)
+             (json)
+             (ice-9 format)
+             (ice-9 pretty-print))
 (include "apis.scm")
 
 (display
@@ -8,18 +11,18 @@
 
 (define* (meaning phrase #:optional (source-lang #:en) (dest-lang #:en))
   "
-  make calls to the glosbe API
+   make calls to the glosbe API
 
   :param phrase: word for which meaning is to be found
   :param source-lang: Defaults to : ""
   :param dest_lang: Defaults to :"" For eg: "" for french
   :returns: returns a json object as str, False if invalid phrase
   "
-  (let ((base-url (assoc #:glosbe base-urls))
-        (url "url"))
-    (display base-url)
-    (newline)
+  (let* ((base-url (assv-ref base-urls #:glosbe))
+         (url (format #f base-url phrase )))
     (display url)
+    (newline)
+    (display (http-get url #:body #f #:streaming? #t))
     (newline)))
 
 (meaning "sam")
