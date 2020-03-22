1ccdb9dc7a474f1fc947c20772f42b9b72aeb7ae
Sergo Pasoevi <spasoevi@gmail.com>
2020-03-22 11:13:08 +0200

    Fix history file path hard-coded

--- c/app.c
+++ c/app.c
@@ -23,9 +23,20 @@
 #include <stdlib.h>
 #include <libguile.h>
 
+char *get_homedir(void)
+{
+    char homedir[MAX_PATH];
+#ifdef _WIN32
+    snprintf(homedir, MAX_PATH, "%s%s", getenv("HOMEDRIVE"), getenv("HOMEPATH"));
+#else
+    snprintf(homedir, MAX_PATH, "%s", getenv("HOME"));
+#endif
+    return strdup(homedir);
+}
+
 char *program_name = "dict";
 char *version = "0.01b";
-char *hist_fname = "/home/sergi/.weehist";
+char *hist_fname = strcat(get_homedir(), "/.weehist");
 
 /* Declare SCM types for calling a function and reading its output */
 SCM func_symbol;
